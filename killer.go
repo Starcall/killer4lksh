a217daac26e2b41e6eca35e956b04fe123d45c3e
starcall <ineedletter@gmail.com>
2021-08-10 16:45:11 +0300

    changing into html templates

--- killer.go
+++ killer.go
@@ -1,90 +1,74 @@
 package main
 
 import (
-    "github.com/gorilla/mux"
-    "fmt"
-    "log"
-    "net/http"
-    "encoding/json"
-    "os"
-    "io"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 type User struct {
-    FirstName string `json:"firstname""`
-    LastName string `json:"lastname""`
-    UID string `json:"uid"`
+	FirstName string `json:"firstname""`
+	LastName  string `json:"lastname""`
+	UID       string `json:"uid"`
 }
 
-var allUsers = []User {
-    User {
-         FirstName: "Sasha",
-         LastName: "Maneev",
-         UID: "1",
-    },
-    User {
-         FirstName: "Bogdan",
-         LastName: "Trubetskoy",
-         UID: "12",
-    },
+type KilledList struct {
+	PageTitle   string
+	killedUsers []User
 }
+
+var allUsers = []User{}
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-    fmt.Fprintf(writer, "<h1>Personal Place</h1>" +
-        "write you unique ID" +
-        "<form action=\"/check\" method=\"POST\">" +
-        "<textarea name=\"body\"></textarea><br>" +
-        "<input type=\"submit\" value=\"Login\">" +
-        "</form>")
+	if request.URL.Path == "/" {
+		request.URL.Path = "./static/index.html"
+	}
 }
 
 func getProfile(user User) string {
-    ret := "<tr>"
-    ret += "<th>" + user.FirstName + "</th>"
-    ret += "<th>" + user.LastName + "</th>"
-    ret += "<th>" + user.UID + "</th>"
-    ret += "</tr>"
-    return ret
+	ret := "<tr>"
+	ret += "<th>" + user.FirstName + "</th>"
+	ret += "<th>" + user.LastName + "</th>"
+	ret += "<th>" + user.UID + "</th>"
+	ret += "</tr>"
+	return ret
+}
+
+func getAllUsers() []User {
+	if len(allUsers) == 0 {
+		file, err := os.Open("./data/user_data.json")
+		if err != nil {
+			log.Fatal(err)
+		}
+		decoder := json.NewDecoder(strings.NewReader(file))
+		for {
+			var user User
+			if err := decoder.Decode(&user); err == io.EOF {
+				break
+			} else if err != nil {
+				log.Fatal(err)
+			}
+			allUsers = append(allUsers, user)
+		}
+	}
+	return allUsers
 }
 
 func checkHandler(writer http.ResponseWriter, request *http.Request) {
-    requestedUid := request.FormValue("body")
-    for _, elem := range allUsers {
-        if elem.UID == requestedUid {
-            file, err := os.Open("./data/" + elem.UID + ".json")
-            if err != nil {
-                file, err = os.Create("./data/" + elem.UID + ".json")
-                if err != nil {
-                    writer.WriteHeader(401)
-                    writer.Write([]byte("Something went wrong"))
-                    log.Fatal(err)
-                    return
-                }
-            }
-            decoder := json.NewDecoder(file)
-            writer.Write([]byte("<table style=\"width:100%\">"))
-            writer.Write([]byte("<tr><th>Firstname</th><th>Lastname</th><th>UID</th></tr>"))
-            for {
-                var killed User
-                if err := decoder.Decode(&killed); err == io.EOF {
-                    break
-                } else if err != nil {
-                    log.Fatal(err)
-                }
-                writer.Write([]byte(getProfile(killed)))
-            }
-            writer.Write([]byte("</table>"))
-            return
-        }
-    }
-    writer.Write([]byte("UID is incorrect"))
+	requestedUid := request.FormValue("unique_id")
 
 }
 
 func main() {
-    router := mux.NewRouter()
-    router.HandleFunc("/", handler).Methods("GET")
-    router.HandleFunc("/check", checkHandler).Methods("POST")
-    log.Fatal(http.ListenAndServe(":8080", router))
+	router := mux.NewRouter()
+	//router.HandleFunc("/", handler).Methods("GET")
+	//router.HandleFi
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/"))).Methods("GET")
+	router.HandleFunc("/check", checkHandler).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
-
