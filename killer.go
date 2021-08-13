9248d86e432f2e5d9f3af5c357562881408c50a6
starcall <ineedletter@gmail.com>
2021-08-13 16:50:07 +0500

    shit

--- killer.go
+++ killer.go
@@ -7,7 +7,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-
+	"sync"
+	"os"
 	"github.com/gorilla/mux"
 )
 
@@ -16,6 +17,7 @@ type User struct {
 	LastName  string   `json:"lastname"`
 	UID       string   `json:"uid"`
 	ToKillUID string   `json:"tokilluid"`
+	IsKilled  bool     `json:"iskilled"`
 	Killed    []string `json:"killed"`
 }
 
@@ -23,6 +25,7 @@ type KilledList struct {
 	PageTitle   string
 	UserUID     string
 	KilledUsers []User
+	TargetName  string
 }
 
 type NoSuchUser struct {
@@ -31,6 +34,7 @@ type NoSuchUser struct {
 }
 
 type UserList struct {
+	Locker sync.Mutex
 	AllUsers []User `json:"knownusers"`
 }
 
@@ -43,6 +47,7 @@ func handler(writer http.ResponseWriter, request *http.Request) {
 }
 
 func getAllUsers() []User {
+	allUsers.Locker.Lock()
 	if len(allUsers.AllUsers) == 0 {
 		file, err := ioutil.ReadFile("./data/user_data.json")
 		if err != nil {
@@ -51,6 +56,7 @@ func getAllUsers() []User {
 		err = json.Unmarshal(file, &allUsers)
 	}
 	fmt.Println(allUsers.AllUsers)
+	allUsers.Locker.Unlock()
 	return allUsers.AllUsers
 }
 
@@ -69,10 +75,13 @@ func checkHandler(writer http.ResponseWriter, request *http.Request) {
 	killedList := KilledList{
 		PageTitle:   "Мои жертвы",
 		UserUID:     requestedUid,
+		TargetName:  "",
 		KilledUsers: []User{},
 	}
 	if exist {
 		killedList.PageTitle = userData.FirstName + " " + userData.LastName;
+		targetUser, _ := isUidExist(userData.ToKillUID)
+		killedList.TargetName = targetUser.FirstName + " " + targetUser.LastName
 		for _, target := range userData.Killed {
 			killedUserData, exists := isUidExist(target)
 			if exists {
@@ -89,24 +98,71 @@ func checkHandler(writer http.ResponseWriter, request *http.Request) {
 
 func nosuchUserHandler(writer http.ResponseWriter, request *http.Request, formId string) {
 	tmpl := template.Must(template.ParseFiles("./static/nosuchuser.html"))
+	fmt.Println(formId)
 	tmpl.Execute(writer, NoSuchUser{
 		PageTitle: "No such user id:",
 		UID:       request.FormValue(formId),
 	})
 }
 
-func processKill(killerUID string, targetUID string) {
-	fmt.Println(killerUID)
-	fmt.Println(targetUID)
-	return
+func isSameUser(userA User, userB User) bool {
+	return userA.UID == userB.UID
 }
 
+func saveJson() {
+	json, _ := json.Marshal(allUsers)
+	f, _ := os.Create("./data/user_data.json")
+	defer f.Close()
+	f.Write(json)
+}
+
+func processKill(killer User, target User) {
+	if isSameUser(killer, target) {
+		return
+	}
+	if killer.ToKillUID != target.UID {
+		return
+	}
+	if target.IsKilled {
+		return
+	}
+	if killer.IsKilled {
+		return
+	}
+	for pos, curUser := range getAllUsers() {
+		if isSameUser(curUser, target) {
+			allUsers.Locker.Lock()
+			allUsers.AllUsers[pos].IsKilled = true
+			allUsers.Locker.Unlock()
+			break
+		}
+	}
+	for pos, curUser := range getAllUsers() {
+		if isSameUser(curUser, killer) {
+			allUsers.Locker.Lock()
+			allUsers.AllUsers[pos].Killed = append(allUsers.AllUsers[pos].Killed, target.Killed...)
+			allUsers.AllUsers[pos].Killed = append(allUsers.AllUsers[pos].Killed, target.UID)
+			allUsers.AllUsers[pos].ToKillUID = target.ToKillUID
+			allUsers.Locker.Unlock()
+			break
+		}
+	}
+	saveJson()
+	return
+}
+	
 func killHandler(writer http.ResponseWriter, request *http.Request) {
 	requestedUID := request.FormValue("killapply")
-	_, exist := isUidExist(requestedUID)
-	if exist {
-		userUID := request.FormValue("UID")
-		processKill(userUID, requestedUID)
+	target, targetExist := isUidExist(requestedUID)
+	if targetExist {
+		userUID := request.FormValue("unique_id")
+		killer, killerExist := isUidExist(userUID)
+		if killerExist {
+			processKill(killer, target)
+			checkHandler(writer, request)
+		} else {
+			nosuchUserHandler(writer, request, "unique_id")
+		}
 	} else {
 		nosuchUserHandler(writer, request, "killapply")
 		return
